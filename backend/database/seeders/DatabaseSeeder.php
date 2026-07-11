3091bb4940131f0c0b61d748ce197419d3a474d3
mgksmv <kasumov82@gmail.com>
2026-07-11 23:28:45 +0300

    feat: create test tasks on main db seed

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -28,5 +29,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'user@example.com',
             'password' => Hash::make('password'),
         ]);
+
+        Task::factory()->count(50)->create();
     }
 }
