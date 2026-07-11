9200d1397339d5e38d1a05adc79893152598722a
mgksmv <kasumov82@gmail.com>
2026-07-11 22:48:31 +0300

    feat: add index page placeholder

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -18,6 +18,8 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
   <AppLayout :breadcrumbs="breadcrumbs">
-
+    <div class="flex justify-between gap-4 items-center pt-4 ps-4 pe-4">
+      <p>Добро пожаловать!</p>
+    </div>
   </AppLayout>
 </template>
